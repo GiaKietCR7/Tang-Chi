827f26dc8928fa65bbcea548ac0b852312757cb5
GiaKietCR7 <85275616+GiaKietCR7@users.noreply.github.com>
2021-06-03 21:10:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Em À!',
+    introTitle: 'Linh À!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao Em cứ đi nhầm đường
     Lạc vào tim Anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'HiHi và HiHi',
-    title: 'Phải chăng Em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng Em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng Linh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Linh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích Anh <33',
     btnNo: 'Không,thằng ất ơ nào ấy :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao Em lại yêu Anh <3',
     btnReply: 'Gửi cho Anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu Em nhiều nhiều 😘😘',
-    messDesc: 'Hết dịch mình đi chơi nhó iu emmmmm 🥰. .',
+    messDesc: 'Hết dịch Linh dắt Kiệt đi ăn nhó 🥰. .',
     btnAccept: 'Okiiiiii lun Nhooo <3',
     messLink: 'https://www.messenger.com/t/100034497675510' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
